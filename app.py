62b71b0f0c7bf6d5f134ac6a40a591520e61aa7c
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2025-06-28 14:27:43 +0800

    change port

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def put_book(book_id, new_title, new_author):
         return jsonify({"message": "Book updated successfully!"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host=0.0.0.0, port=5000)
