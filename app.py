f4cf6b5a377dbfb832472bfa40fbb9aaae14a6f7
bautista-johnmatthew <131367336+bautista-johnmatthew@users.noreply.github.com>
2025-07-05 12:58:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     """Return the instructions for using the API."""
+    init_db()
     return "Welcome to the Book API! Use /books to get a list of books."
 
 @app.route('/books', methods=['GET'])
 def get_books():
     dictionary_books = []
     
-    if not view_books():
+    if view_books() == "No books found in the database.":
         return jsonify({"message": "No books found in the database."}), 404
 
     for book in view_books():
